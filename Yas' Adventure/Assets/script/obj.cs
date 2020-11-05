bad09c20026ddf49051462af19fbafe6d41d4cf4
leenuu <kangjoon10@gmail.com>
2020-11-05 14:56:49 +0900

    base

---
+++ Yas' Adventure/Assets/script/obj.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject cam_obj;
+    private float obj_speed = 12.0f;
+    private float d_time = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        d_time += Time.deltaTime;
+        if(d_time >= 0.8f)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.left * Time.deltaTime * obj_speed);
+    }
+}
---
+++ Yas' Adventure/Assets/script/test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject obj;
+    
+    //private Vector2 vec_obj = new Vector2(1.0f, 0.0f); 
+    private float fire_del = 2.0f;
+
+    void Start()
+    {
+        //Instantiate(obj, transform.position, Quaternion.identity); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fire();
+    }
+
+    void fire()
+    {
+        fire_del += Time.deltaTime;
+        if(fire_del >= 2.0f)
+        {
+            Instantiate(obj, transform.position, Quaternion.identity); 
+            fire_del = 0;
+        }
+    }
+}
