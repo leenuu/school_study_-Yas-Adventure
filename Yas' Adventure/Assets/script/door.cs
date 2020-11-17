083a63ac8e85a09021a51e8a9cd648f7c224f5cd
Moonshinhyuk <73520866+Moonshinhyuk@users.noreply.github.com>
2020-11-18 03:24:59 +0900

    motion
    good

---
+++ Yas' Adventure/Assets/script/door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animator.SetBool("ismonster",false);
+    }
+
+
+    void Update()
+    {
+        ismonster();
+    }
+
+    void ismonster(){
+        if(true==true)
+        {
+            animator.SetBool("ismonster",true);
+        }
+        else{
+            animator.SetBool("ismonster",false);
+        }
+    }
+}
